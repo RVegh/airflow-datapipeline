72c2926e5008fd23a1da1245a15fcd5086e31f0e
Vegh <RVegh>
2022-03-28 13:59:56 -0300

    extracting data from twitter api and saving it on local folder

--- airflow/plugins/operators/twitter_operator.py
+++ airflow/plugins/operators/twitter_operator.py
@@ -1,16 +1,23 @@
 from textwrap import indent
+
 from airflow.models import BaseOperator, DAG, TaskInstance
 from airflow.utils.decorators import apply_defaults
 from hooks.twitter_hook import TwitterHook
+
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+from pathlib import Path
+from os.path import join
 
 class TwitterOperator(BaseOperator):
+
+    template_fields = ['query', 'file_path', 'start_time', 'end_time']
 
     @apply_defaults
     def __init__(
         self, 
         query,
+        file_path,
         conn_id=None,
         start_time=None,
         end_time=None,
@@ -18,9 +25,13 @@ class TwitterOperator(BaseOperator):
     ):
         super().__init__(*args, **kwargs)
         self.query = query
+        self.file_path = file_path
         self.conn_id = conn_id
         self.start_time = start_time
         self.end_time = end_time
+    
+    def create_parent_folder(self):
+        Path(Path(self.file_path).parent).mkdir(parents=True, exist_ok=True)
 
     def execute(self, context):
         hook = TwitterHook(
@@ -29,13 +40,22 @@ class TwitterOperator(BaseOperator):
             start_time = self.start_time,
             end_time = self.end_time
         )
-        for pg in hook.run():
-            print(json.dumps(pg, indent=4, sort_keys=True))
+        self.create_parent_folder
+        with open(self.file_path, 'w') as output_file:
+            for pg in hook.run():
+                json.dump(pg, output_file, ensure_ascii=False)
+                output_file.write('\n')
 
 if __name__ == "__main__":
     with DAG(dag_id='TwitterTest', start_date=datetime.now()) as dag:
         to = TwitterOperator(
-            query = 'AluraOnline', task_id = 'test_run'
+            query = 'AluraOnline', 
+            file_path=join(
+                '/home/vegh/Desktop/Estudos/airflow-datapipeline/datalake',
+                'twitter_aluraonline',
+                'extract_date={{ ds }}',
+                'AluraOnline_{{ ds_nodash }}.json'),
+            task_id = 'test_run'
         )
-        ti = TaskInstance(task=to, execution_date=datetime.now())
-        to.execute(ti.get_template_context())
+        ti = TaskInstance(task=to, execution_date=datetime.now() - timedelta(days=1))
+        ti.run()
